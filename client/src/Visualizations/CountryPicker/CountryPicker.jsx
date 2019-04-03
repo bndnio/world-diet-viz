a8a826cd2d04eae9fe0a991e6506eb2bb8aaa87e
Brendon Earl <bearl@uvic.ca>
2019-04-03 15:58:51 -0700

    Build country picker

--- client/src/Visualizations/CountryPicker/CountryPicker.jsx
+++ client/src/Visualizations/CountryPicker/CountryPicker.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withData } from '../../DataContext/withData';
+import { Select } from 'antd';
+
+const Option = Select.Option;
 
 class CountryPicker extends Component {
+  static propTypes = {
+    countries: PropTypes.arrayOf(PropTypes.string.isRequired),
+  };
+
+  static defaultProps = {
+    countries: ['Canada', 'Murica', 'China', 'Mexico', 'Costa Rica'],
+  };
+
+  handleChange(value) {
+    console.log(`selected ${value}`);
+  }
+
   render() {
-    return <div>CountryPicker</div>;
+    const { countries } = this.props;
+
+    return (
+      <Select
+        mode="multiple"
+        style={{ width: '100%' }}
+        placeholder="Please select"
+        defaultValue={[]}
+        onChange={this.handleChange}
+      >
+        {countries.map(country => (
+          <Option key={country}>{country}</Option>
+        ))}
+      </Select>
+    );
   }
 }
 
-CountryPicker.propTypes = {};
-
-export default CountryPicker;
+export default withData(CountryPicker);
