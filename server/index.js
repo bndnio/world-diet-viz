14cbbb84041c2449089b4697da403d848a28488d
Brendon Earl <bearl@uvic.ca>
2019-04-05 15:13:38 -0700

    enable playground in production

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const db = require('./db');
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer(schema);
+const server = new ApolloServer({
+  ...schema,
+  introspection: true,
+  playground: true,
+});
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
