502948da293d831e67c95399d59ca20fa1c0a38e
Brendon Earl <bearl@uvic.ca>
2019-04-12 11:17:45 -0700

    Add macronutrient map module

---
+++ client/src/Modules/MacroNameMap.js
@@ -0,0 +1,8 @@
+export default {
+  'Grand Total - Animal Protein - Food supply': 'Animal Protein',
+  'Grand Total - Carbs - Food supply': 'Carbs',
+  'Grand Total - Fat - Food supply': 'Fat',
+  'Grand Total - Food supply': 'Grand Total',
+  'Grand Total - Plant Protein - Food supply': 'Plant Protein',
+  'Grand Total - Protein - Food supply': 'Protein',
+};
