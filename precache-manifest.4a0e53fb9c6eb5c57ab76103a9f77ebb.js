a989f5e238f93614800c5ca88a6a9d4530f9b0cf
Brendon Earl <bearl@uvic.ca>
2019-04-13 15:07:15 -0700

    Updates

---
+++ precache-manifest.4a0e53fb9c6eb5c57ab76103a9f77ebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c93a1151559b88874711",
+    "url": "/static/js/main.9234c132.chunk.js"
+  },
+  {
+    "revision": "b62475859af0bcf8a835",
+    "url": "/static/js/2.68f64cbb.chunk.js"
+  },
+  {
+    "revision": "c93a1151559b88874711",
+    "url": "/static/css/main.9f032d64.chunk.css"
+  },
+  {
+    "revision": "b62475859af0bcf8a835",
+    "url": "/static/css/2.021b1766.chunk.css"
+  },
+  {
+    "revision": "23ca2bc4040995c9d45fda8c47390c56",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bbbf74deefabd4132e0372cc4755407c.js"
+  "/precache-manifest.4a0e53fb9c6eb5c57ab76103a9f77ebb.js"
 );
 
 workbox.clientsClaim();
